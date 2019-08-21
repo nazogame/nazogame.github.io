64a0d2dc2ef158bd68d4f9da93a32511d263ba5d
nazogame <54229460+nazogame@users.noreply.github.com>
2019-08-21 16:14:43 +0800

    Create func-2.js

---
+++ JsCode/func-2.js
@@ -0,0 +1,12 @@
+function check2(str){
+	return Base64.encode(str)==psw2();
+}
+function que2(){
+	return "什么是世界上最伟大的质数？";
+}
+function psw2(){
+	return "MTkyNjA4MTc=";
+}
+function tip2(){
+	return "过于简单，不用提示。";
+}
